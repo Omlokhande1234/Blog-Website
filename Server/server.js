cd2cafbf43e5685dbc74e985563171c1d3aa8c5d
Omlokhande1234 <olokhande56@gmail.com>
2024-03-08 19:17:22 +0530

    created backend Server

---
+++ Server/server.js
@@ -0,0 +1,7 @@
+import app from './app.js'
+import dotenv from  'dotenv';
+dotenv.config();
+const PORT=process.env.PORT||3000
+app.listen(PORT,()=>{
+    console.log(`Server is running at port ${PORT}`)
+})
