18e451fc739400314dc3d9dc325d4e52262f528e
Omlokhande1234 <olokhande56@gmail.com>
2024-03-16 14:58:59 +0530

    created errorMiddleware

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -1,5 +1,6 @@
 import users from '../Models/User.js'
 import bcryptjs from 'bcryptjs'
+import errorhandler from '../utils/errorUtils.js'
 import jwt  from 'jsonwebtoken'
 
 let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
@@ -9,36 +10,20 @@ export const SignUp=async(req,res)=>{
     const{fullname,email,password}=req.body
     const hashPassword=await bcryptjs.hash(password,10)
     if(fullname.length<3){
-        return res.status(400).json({
-            success:"false",
-            error:'Full name must be at least 3 characters long.'
-            
-        })
+        return next(errorhandler(400,'Full name should be at least three characters long'))
     }
     if(!email.length){
-        return res.status(400).json({
-            success:"false",
-            error: "Email is required."
-        })
+      return next(errorhandler(400,"Email is required"))
     }
     if(!password.length){
-        return res.status(400).json({
-            success:"false",
-            error:"Password is required"
-        })
+        return next(errorhandler(400,"Password is required"))
     }
     if(!emailRegex.test(email)){
-        return res.status(400).json({
-            success:"false",
-            error:"Email is invalid"
-        })
+        return next(errorhandler(400,'Enter valid email'))
 
     }
     if(!passwordRegex.test(password)){
-        return res.status(400).json({
-            success:"false",
-            error:"password is invalid"
-        })
+        return next(errorhandler(400,'Enter valid password'))
     }
     let username=email.split("@")[0]
     const newUser=new users({
@@ -58,15 +43,23 @@ export const SignUp=async(req,res)=>{
         );
      } catch (error) {
          if(error.code == 11000) {
-             return res.status(500).json({
-                 success:false,
-                 error:"Email is already exists!"
-             })
+             return next(errorhandler(500,'Email already exxists'))
          }
         return res.status(404).json(error.message);
      }
 
 }
 
-export const SignIn=async(req,res)=>{}
+export const SignIn=async(req,res,next)=>{
+    const{email,password}=req.body
+   try{
+    if(!email||!password){
+
+    }
+    const user= await users.findOne({'personal_info.email':email})
+   }catch(error){
+
+   }
+
+}
 export const LogOut=async(req,res)=>{}
---
+++ Server/utils/errorUtils.js
@@ -0,0 +1,6 @@
+const errorhandler=(statuscode,message)=>{
+    const error=new Error()
+    error.statuscode=statuscode,
+    error.message=message
+    return error
+}
